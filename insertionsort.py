f490e5cc0807f689c5b3f729ec1778ff11a63627
AmitkumarRawat15 <ravatamit45@gmail.com>
2022-12-17 12:39:11 +0530

    Insertion Sort

---
+++ insertionsort.py
@@ -0,0 +1,12 @@
+def insertionSort(mylist):
+    for i in range(1,len(mylist)):
+        anchor = mylist[i]
+        j = i - 1
+        while j>=0 and anchor<mylist[j]:
+            mylist[j+1]=mylist[j]
+            j = j - 1
+        mylist[j+1]=anchor
+        
+mylist=[21,38,1,46,37,95,47,76]
+insertionSort(mylist)
+print(mylist)
